e013bf26a9bf2ac4b64fc600dfa7fbf4712fe61e
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-05-22 01:39:32 +0500

    Assigned

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Events :
 
-const email = document.querySelector("#email");
-const pass = document.querySelector("#password");
-const form = document.querySelector("#form");
+const email = document.querySelector('#email');
+const pass = document.querySelector('#password');
+const form = document.querySelector('#form');
 
 
-form.addEventListener("submit" , function()
-{
+form.addEventListener('submit' , function(Events)
+{    
+    Events.preventDefault();
     console.log(email.value);
     console.log(pass.value);
 })
