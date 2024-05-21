c9a3ed3077fa7ccada1cee0c0a6d88bb4cddb8ca
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-05-22 00:50:06 +0500

    Assigned

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // Events :
+
+let email = document.querySelector(".email");
+let pass = document.querySelector(".password");
+
+
+email.addEventListener("click" , function(){
+    console.log(email);
+}
+)
+
+pass.addEventListener("click" , function(){
+    console.log(pass);
+}
+)
